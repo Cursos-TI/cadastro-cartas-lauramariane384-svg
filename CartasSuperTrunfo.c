9c555256eaf035a9834895da861dcdc13ed1e240
lauramariane384-svg <lauramariane384@gmail.com>
2026-05-11 17:44:16 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 
 // Desafio Super Trunfo - Países
-// Tema 2 - Cadastro das cartas
-// Objetivo: No nível aventureiro, o jogador deve cadastrar as cartas do jogo, inserindo as propriedades de cada cidade. O programa deve permitir a entrada de dados para pelo menos 2 cartas, e exibir as informações cadastradas ao final..
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
@@ -14,6 +12,8 @@ int populacao1;
 float area1;
 float pib1;
 int pontos1;
+float poder1;
+float inverso_densidade1;
 
 //Carta 02
 char estado2;
@@ -23,6 +23,8 @@ int populacao2;
 float area2;
 float pib2;
 int pontos2;
+float poder2;
+float inverso_densidade2;
 
   // Área para entrada de dados
 // === ENTRADA CARTA 1 ===
@@ -49,9 +51,10 @@ scanf("%f", &pib1);
 printf("Pontos turisticos: ");
 scanf("%d", &pontos1);
 
-float Densidade1 = populacao1 / area1; // Cálculo da densidade populacional para a carta 1
-
-float PIB_per_capita1 = pib1 / populacao1; // Cálculo do PIB per capita para a carta 1
+float Densidade1 = (float) populacao1 / area1; // Cálculo da densidade populacional para a carta 1
+float PIB_per_capita1 = (float) pib1 / populacao1; // Cálculo do PIB per capita para a carta 1
+float inverso_densidade1 = 0.1f / Densidade1; // Cálculo do inverso da densidade populacional para a carta 1
+float poder1 = (float) populacao1 + area1 + pib1 + (float) pontos1 + inverso_densidade1; // Cálculo do poder da carta 1
 
 // === ENTRADA CARTA 2 ===
 printf("\n== Cadastro Carta 2 ==\n");
@@ -77,9 +80,10 @@ scanf("%f", &pib2);
 printf("Pontos turisticos: ");
 scanf("%d", &pontos2);
 
-float Densidade2 = populacao2 / area2; // Cálculo da densidade populacional para a carta 2
-
-float PIB_per_capita2 = pib2 / populacao2; // Cálculo do PIB per capita para a carta 2
+float Densidade2 = (float) populacao2 / area2; // Cálculo da densidade populacional para a carta 2
+float PIB_per_capita2 = (float) pib2 / populacao2; // Cálculo do PIB per capita para a carta 2
+float inverso_densidade2 = 0.1f / Densidade2; // Cálculo do inverso da densidade populacional para a carta 2
+float poder2 = (float) populacao2 + area2 + pib2 + (float) pontos2 + inverso_densidade2; // Cálculo do poder da carta 2
 
   // Área para exibição dos dados da cidade
 printf("\n\n=== CARTAS CADASTRADAS ===\n");
@@ -93,6 +97,7 @@ printf("Area: %.2f\n", area1);
 printf("PIB: %.2f\n", pib1);
 printf("Densidade Populacional: %.2f\n", Densidade1);
 printf("PIB per Capita: %.2f\n", PIB_per_capita1);
+printf("Poder da Carta: %.2f\n", poder1);
 
 printf("\nCarta 2:\n");
 printf("Estado: %c\n", estado2);
@@ -103,6 +108,18 @@ printf("Area: %.2f\n", area2);
 printf("PIB: %.2f\n", pib2);
 printf("Densidade Populacional: %.2f\n", Densidade2);
 printf("PIB per Capita: %.2f\n", PIB_per_capita2);
+printf("Poder da Carta: %.2f\n", poder2);
+
+// Comparação entre as cartas
+printf("\n\n=== COMPARACAO ENTRE AS CARTAS ===\n");
+
+if (poder1 > poder2) {
+    printf ("Carta 1 é mais poderosa que Carta 2.\n");
+} else if (poder2 > poder1) {
+    printf ("Carta 2 é mais poderosa que Carta 1.\n");    
+} else {
+    printf ("Carta 1 e Carta 2 têm o mesmo poder.\n");
+}
 
 return 0;
 } 
